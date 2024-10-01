6e0ac3e867b2667387277d6986ffb8b332540833
Hiago Braga <hiagoaugustobraga@gmail.com>
2024-09-30 21:25:30 -0300

    fetch: Importing Component Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Recommendations from './components/Recommendations.js';
+import TopRated from './components/TopRated.js';
+import RecentReleases from './components/RecentReleases.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Document Prime</h1>
       </header>
+      <main>
+        <Recommendations />
+        <TopRated />
+        <RecentReleases />
+      </main>
     </div>
   );
 }
 
 export default App;
+
